61455b79b677f462a74a23fb7f57dfb82e41d238
Abdallah Ayman <149713232+vAbdallahv@users.noreply.github.com>
2024-04-21 18:47:30 +0000

    ..

--- KeyExpansion.v
+++ KeyExpansion.v
@@ -2,7 +2,7 @@
 module KeyExpansion (initial_key,full_keys);
 
 input [127:0] initial_key;
-output [1407:0] full_key;
+output [1407:0] full_keys;
 
 function [31:0] RotWord (input [31:0] Word);
     RotWord = {Word[23:0],Word[31:24]};
