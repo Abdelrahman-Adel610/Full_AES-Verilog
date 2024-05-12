c15f433d886d0b8916f5293466755de1f6487141
Abdelrahman Adel Hashiem <149705123+Abdelrahman-Adel610@users.noreply.github.com>
2024-05-12 14:23:01 +0300

    Added comments

--- DEMO/AES_128.v
+++ DEMO/AES_128.v
@@ -1,29 +1,28 @@
- `include "ShiftRows.v"
- `include "SubBytes.v"
- `include "InvShiftRows.v"
- `include "InvSubBytes.v"
- `include "Inv_MixColumns.v"
- `include "display.v"
- `include "MixColumns.v"
- `include "AddRoundKey.v"
- `include "KeyExpansion.v"
+//`include "ShiftRows.v"
+`include "SubBytes.v"
+`include "InvShiftRows.v"
+`include "InvSubBytes.v"
+`include "Inv_MixColumns.v"
+`include "display.v"
+`include "MixColumns.v"
+`include "AddRoundKey.v"
+`include "KeyExpansion.v"
 
-module AES #(parameter nr=10,parameter nw=4)(input[127:0] in,input[32*nw-1:0] Key, output reg flag,output wire [20:0] sevSeg,input clk,input reset);
+module AES_128( output reg flag,output wire [20:0] sevSeg,input clk);
 reg[4:0] counter;
 wire [11:0] bcd;
 reg [127:0] cipher;
-reg rst;
-/*
-wire [127:0] in;
-wire [127:0] Key;
-*/
-wire [((nr + 1) * 128 - 1) : 0] full_key;
-
-reg [127:0] initialstate;
+reg [127:0] in;
+reg [127:0] Key;
+wire [1407:0] full_key;
+reg [127:0] state;
 reg [127:0] Current_key;
 wire [127:0] Current_keywire;
 
 reg [127:0]SubBytesIn;
+reg [127:0]ShiftRowsIn;
+reg [127:0]MixColumnsIn;
+reg [127:0]AddRoundKeyIn;
 
 wire [127:0]SubBytesOutwire;
 wire [127:0]ShiftRowsOutwire;
@@ -31,115 +30,161 @@ wire [127:0]MixColwire;
 wire [127:0]AddRoundKey_out;
 
 
+reg [127:0]invSubBytesIn;
 reg [127:0]invShiftRowsIn;
+reg [127:0]invMixColumnsIn;
+reg [127:0]invAddRoundKeyIn;
 
 wire [127:0]invSubBytesOutwire;
 wire [127:0]invShiftRowsOutwire;
 wire [127:0]invMixColwire;
 wire [127:0]invAddRoundKey_out;
+
 reg [7:0]displayOut;
 
 
-KeyExpansion #(nr,nw)kr(Key , full_key);
+KeyExpansion kr(Key , full_key);
+
 
 initial
 begin
- flag=0;
- counter<=1;
+ in='h3243f6a8885a308d313198a2e0370734;
+ Key='h2b7e151628aed2a6abf7158809cf4f3c;
+ counter=-1;
+ flag=1'b0;
+#1;
+state=in^full_key[127:0];
+displayOut=state[7:0];
 end
 
 
-//The next always block is  responsible for getting the bits to be displayed each round
-always@(*)
-begin
-rst=reset;
-  
-  if(counter==1)
-  begin
-  displayOut<=in[7:0]; 
-  end
-  
-  if(counter>1 && counter<nr+1)        																							   
-  displayOut<=AddRoundKey_out[7:0];
-
-  if(counter==nr+1)
-  begin
-  displayOut<=ShiftRowsOutwire[7:0]^full_key[((nr+1)*128-121)-:8];/////////////////////
-end
-
-  if(counter>nr+1&&counter<2*nr+1)
-  begin 
-  displayOut<=invMixColwire[7:0];
-  end
-
-  if(counter==2*nr+1)
-  begin
-  displayOut<=invAddRoundKey_out;
-  cipher<=invAddRoundKey_out;
-  flag=(invAddRoundKey_out==in)?1'b1:1'b0;
-  end
-
-  //flag=(invAddRoundKey_out==in)?1'b1:1'b0;
-
-end
-
-//The next always block is responsible for setting the proper input for each round [1-10]==>ENCRP [11-21]==>DECRP
-always@(negedge clk)
-begin
-if(rst==1)
-begin
-counter<=1;
-end
-else
-begin
-if(counter>nr+1)  Current_key<=full_key [((counter+1-(nr+1))*(-128)+((nr+1)*128-1))-:128] ;
-else             Current_key<= full_key[(counter*(128)+127)-:128]; 
-
- if(counter==1)
-begin
-//   Current_key<= full_key[(counter*(128)+127)-:128]; 
- SubBytesIn<=in^full_key[127:0];
-end
-else if(counter>1&&counter<=nr)          //during Encryption
-begin
-  // Current_key<= full_key[(counter*(128)+127)-:128]; 
-   SubBytesIn<=AddRoundKey_out;
-end
-
-if(counter==nr+1)                         //The first round of Decryption
-begin
-  cipher<=ShiftRowsOutwire^Current_key; //setting the Encryption output cipher 
-  invShiftRowsIn<=ShiftRowsOutwire^full_key[((nr+1)*128-1)-:128]^Current_key;
-  Current_key<=full_key [((counter+1-(nr+1))*(-128)+((nr+1)*128-1))-:128] ;
-end
-else if(counter>nr+1&&counter<2*nr+1)         //during Decryption
-begin
-  //Current_key<=full_key [((counter+1-(nr+1))*(-128)+(352*nw-1))-:128] ;
-  invShiftRowsIn<=invMixColwire;
-end
-if(counter<2*nr+2)
- counter<=counter+1;
-
- end
-end
-
-
-SubBytes su (SubBytesIn, SubBytesOutwire);
-ShiftRows sr(SubBytesOutwire,ShiftRowsOutwire);
-MixColumns mc(ShiftRowsOutwire,MixColwire);
-AddRoundKey AK (MixColwire , AddRoundKey_out , Current_key);
-
-
-
-InvShiftRows  isr (invShiftRowsIn,invShiftRowsOutwire);
-InvSubBytes  isb (invShiftRowsOutwire, invSubBytesOutwire);
-AddRoundKey ark(invSubBytesOutwire , invAddRoundKey_out , Current_key);
-Inv_MixColumns  imc (invAddRoundKey_out,invMixColwire);
-
-
-binary_to_bcd DS(displayOut[7:0],bcd);
+binary_to_bcd DS(AddRoundKey_out[7:0],bcd);
 seven_seg seg1(bcd[11-:4],sevSeg[20-:7]);
 seven_seg seg2(bcd[7-:4],sevSeg[13-:7]);
 seven_seg seg3(bcd[3-:4],sevSeg[6-:7]);
+
+
+always@(posedge clk)
+begin
+
+  counter=counter+1;
+
+  if(counter<11 &&counter>0)
+  begin
+
+    if(counter==1)
+    begin
+      	#1;
+         SubBytesIn<=state;
+        #1;
+        ShiftRowsIn<=SubBytesOutwire;
+        #1;
+        MixColumnsIn<=ShiftRowsOutwire;
+        #1;
+       Current_key<= full_key[(counter*(128)+127)-:128];
+       AddRoundKeyIn<=MixColwire;
+    end
+
+   else if(counter==10)
+    begin
+      	#1;
+        SubBytesIn<=AddRoundKey_out;
+        #1;
+        ShiftRowsIn<=SubBytesOutwire;
+        // #1;
+        // MixColumnsIn<=ShiftRowsOutwire;
+        #1;
+        Current_key<= full_key[(counter*(128)+127)-:128];
+        AddRoundKeyIn<=ShiftRowsOutwire;
+      	#1;
+        cipher<=AddRoundKey_out;
+        // #1;
+        // displayOut=cipher[7:0];
+    end
+
+  else if(counter>1 && counter<10)
+    begin
+      	#1;
+        SubBytesIn<=AddRoundKey_out;
+        #1;
+        ShiftRowsIn<=SubBytesOutwire;
+        #1;
+        MixColumnsIn<=ShiftRowsOutwire;
+        #1;
+        Current_key<= full_key[(counter*(128)+127)-:128];
+        AddRoundKeyIn<=MixColwire;      
+    end
+   
+   #1;
+    displayOut=AddRoundKey_out[7:0];
+
+  end
+
+else if(counter>=11 && counter<21)
+  begin 
+
+  if(counter==11)
+  begin 
+    #1;
+   Current_key=full_key[1407-:128];
+   
+   invAddRoundKeyIn<=AddRoundKey_out;
+   #1;
+   invShiftRowsIn<=invAddRoundKey_out;
+   #1;
+   invSubBytesIn<=invShiftRowsOutwire;
+#1;
+   displayOut<=invSubBytesOutwire[7:0];
+  end
+
+  else if(counter>11 && counter<21)
+  begin
+      Current_key<=full_key [((counter-11)*(-128)+1407)-:128] ;
+      invAddRoundKeyIn<=invSubBytesOutwire;
+      #1;
+      invMixColumnsIn<=invAddRoundKey_out;
+      #1;
+      invShiftRowsIn<=invMixColwire;
+      #1;
+      invSubBytesIn<=invShiftRowsOutwire;
+      #1;
+  end
+  if(counter>=11)
+displayOut<=invSubBytesOutwire[7:0];
+  end
+  else if(counter==21)
+  begin 
+    cipher=invSubBytesOutwire^full_key[127:0];
+    if(cipher==in)
+      flag=1'b1;
+    else
+      flag=1'b0;
+  end
+
+else
+begin
+
+end
+
+ 
+
+  end
+ 
+
+SubBytes su (SubBytesIn, SubBytesOutwire);
+ShiftRows sr(ShiftRowsIn,ShiftRowsOutwire);
+MixColumns mc(MixColumnsIn,MixColwire);
+AddRoundKey AK (AddRoundKeyIn , AddRoundKey_out , Current_key);
+
+
+
+
+
+AddRoundKey op1(invAddRoundKeyIn , invAddRoundKey_out , Current_key);
+Inv_MixColumns  op2 (invMixColumnsIn,invMixColwire);
+InvShiftRows  op3 (invShiftRowsIn,invShiftRowsOutwire);
+InvSubBytes  op4 (invSubBytesIn, invSubBytesOutwire);
+
+
 
 endmodule
